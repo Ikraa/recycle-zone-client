77caa0975230f51bc5a9f5024ea341b28cc000d2
munnisaikra <munnisaikra750@gmail.com>
2023-01-10 21:18:04 +0600

    added nabvar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
-import logo from "./logo.svg";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/Home/Home";
+import Navbar from "./Shared/Navbar/Navbar";
 
 function App() {
-  return <div></div>;
+  const routeLink = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+    },
+  ]);
+  return (
+    <>
+      {
+        <RouterProvider
+          router={routeLink}
+          fallbackElement={<h1>Loading...</h1>}
+        />
+      }
+    </>
+  );
 }
 
 export default App;
---
+++ src/Shared/Navbar/Navbar.js
@@ -0,0 +1,93 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <>
+      <div className="bg-[#f76b8a] min-h-[60px] flex items-center px-8">
+        <ul className="flex items-center ">
+          <span className="text-gray-900  font-bold">
+            <Link
+              to="/"
+              className="hover:bg-black duration-300 transition-all text-[11px]  rounded-[3px] text-[#f76b8a] mr-[10px] px-3 py-1 bg-[#eaf6f6]"
+              href="/home"
+            >
+              Home
+            </Link>
+          </span>
+          <span className="text-gray-900  ">
+            <a className=" mr-[10px] px-3 py-1 text-white" href="/home">
+              |
+            </a>
+          </span>
+          <span className="text-gray-900  font-bold">
+            <Link
+              to="/"
+              className="hover:bg-black duration-300 transition-all text-[11px]  rounded-[3px] text-[#f76b8a] mr-[10px] px-3 py-1 bg-[#eaf6f6]"
+              href="/home"
+            >
+              Blog
+            </Link>
+          </span>
+          <span className="text-gray-900  ">
+            <a className=" mr-[10px] px-3 py-1 text-white" href="/home">
+              |
+            </a>
+          </span>
+          <span className="text-gray-900  font-bold">
+            <Link
+              to="/"
+              className="hover:bg-black duration-300 transition-all text-[11px]  rounded-[3px] text-[#f76b8a] mr-[10px] px-3 py-1 bg-[#eaf6f6]"
+              href="/home"
+            >
+              Dashboard
+            </Link>
+          </span>
+          <span className="text-gray-900  ">
+            <a className=" mr-[10px] px-3 py-1 text-white" href="/home">
+              |
+            </a>
+          </span>
+          <span className="text-gray-900  font-bold">
+            <Link
+              to="/"
+              className="hover:bg-black duration-300 transition-all text-[11px]  rounded-[3px] text-[#f76b8a] mr-[10px] px-3 py-1 bg-[#eaf6f6]"
+              href="/home"
+            >
+              Login
+            </Link>
+          </span>
+          <span className="text-gray-900  ">
+            <a className=" mr-[10px] px-3 py-1 text-white" href="/home">
+              |
+            </a>
+          </span>
+          <span className="text-gray-900  font-bold">
+            <Link
+              to="/"
+              className="hover:bg-black duration-300 transition-all text-[11px]  rounded-[3px] text-[#f76b8a] mr-[10px] px-3 py-1 bg-[#eaf6f6]"
+              href="/home"
+            >
+              About
+            </Link>
+          </span>
+          <span className="text-gray-900  ">
+            <a className=" mr-[10px] px-3 py-1 text-white" href="/home">
+              |
+            </a>
+          </span>
+          <span className="text-gray-900  font-bold">
+            <span
+              className="hover:bg-black duration-300 transition-all text-[11px]  rounded-[3px] text-[#f76b8a] mr-[10px] px-3 py-1 bg-[#eaf6f6]"
+              href="/home"
+            >
+              Log Out
+            </span>
+          </span>
+        </ul>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
