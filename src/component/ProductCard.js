2de2fde062ae81aefe4ce735a53c3cf9899c5da9
munnisaikra <munnisaikra750@gmail.com>
2023-01-11 00:31:06 +0600

    added card and image

--- src/component/ProductCard.js
+++ src/component/ProductCard.js
@@ -14,15 +14,19 @@ const ProductCard = ({ item }) => {
             alt=""
           />
           <h4 className="text-white my-2 text-2xl text-start">{item?.name}</h4>
-          <p className="text-start text-gray-400">{item?.description}</p>
-          <p className="text-start text-2xl">
+          <p className="text-start text-gray-500">{item?.description}</p>
+          <h6 className="text-start text-white text-2xl mt-2">
             Original Price:${item?.originalPrice}
-          </p>
-          <p className="text-start text-2xl">
+          </h6>
+          <h6 className="text-start text-2xl text-white">
             Resale Price:{item?.resalePrice}
-          </p>
-          <h6 className="text-start text-2xl">User:{item?.used} Years</h6>
-          <p className="text-start text-2xl">Post Date:{item?.postDate}</p>
+          </h6>
+          <h6 className="text-start text-2xl text-white">
+            User:{item?.used} Years
+          </h6>
+          <h5 className="text-start text-2xl text-white">
+            Post Date:{item?.postDate}
+          </h5>
           <p className="text-start ">
             Seller: {item?.sellerName}{" "}
             {item?.verified && (
@@ -30,7 +34,7 @@ const ProductCard = ({ item }) => {
             )}
           </p>
 
-          <a href="#">Read More</a>
+          <a>Book Now</a>
         </div>
       </div>
     </div>
